d9a1810406236d3d9a98343517ad773747479fe1
Harshil Shah <hsharshil@gmail.com>
2021-11-26 06:20:32 -0500

    Implemented Main menu and individual ticket viewing functionality

--- zendesk.py
+++ zendesk.py
@@ -35,7 +35,7 @@ def display_page(page_count, total_pages, json_obj):
         display_ticket(index, json_obj)
 
 def take_input(page_count, total_pages):
-    choice = input("\nPress - | r:Next Page | l:Previous Page | e:Exit |")
+    choice = input("\nPress - | r:Next Page | l:Previous Page | e:Exit to Main Menu | -\t")
     if choice=='r':
         if page_count<total_pages-1:
             page_count+=1 
@@ -105,13 +105,39 @@ def run():
     total_pages = math.ceil(len(json_obj["tickets"])/25)
     
     while(True):
-        
-        display_page(page_count, total_pages, json_obj)
-        choice, page_count = take_input(page_count, total_pages)
-        
-        if choice=='e':
-            print("\nExit Successful")
+        main_choice = input("\nMain Menu: Select your viewing choice - \n| 1:View All Tickets |\n| 2:View latest ticket |\n| 3:Select ticket by ID |\n| e:Exit |\n")
+        if main_choice=='1':
+            print("\nViewing all Tickets:")
+            while(True):
+                
+                display_page(page_count, total_pages, json_obj)
+                choice, page_count = take_input(page_count, total_pages)
+                
+                if choice=='e':
+                    print("\nExit to Main Menu Successful")
+                    break
+                
+        elif main_choice=='2':
+            print ("\n{:<12} {:<12} {:<30}".format('Ticket ID','Status','Subject'))
+            display_ticket(len(json_obj["tickets"])-1, json_obj)
+            
+        elif main_choice=='3':
+            flag = 0
+            select_id = int(input("\nEnter required ticket ID:\t"))
+            for index in range(len(json_obj["tickets"])):
+                if(json_obj["tickets"][index]["id"]==select_id):
+                    print ("\n{:<12} {:<12} {:<30}".format('Ticket ID','Status','Subject'))
+                    display_ticket(index, json_obj)
+                    flag = 1
+            if flag==0:
+                print(f'\nTicket with ID = {select_id} not found')
+                
+        elif main_choice=='e':
+            print("\nExit Successful\nThank you for using my Ticket Viewer and have an awesome day!")
             break
+        
+        else:
+            print("\nPlease enter a valid input choice")
     
     return 1
 
